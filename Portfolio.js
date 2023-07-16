c6024a104ce0c3e44828261aa694d608cbc7927a
Siuli Mandal <83899473+siuli13@users.noreply.github.com>
2023-07-17 02:06:50 +0530

    Add files via upload

---
+++ Portfolio.js
@@ -0,0 +1,29 @@
+
+document.addEventListener('DOMContentLoaded', function () {
+    const checkbox = document.querySelector('input[name="checkbox"]');
+    const menuBtn = document.getElementById('menu');
+
+    menuBtn.addEventListener('click', function () {
+        checkbox.checked = !checkbox.checked;
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    const hireMeBtn = document.querySelector('.hire-me-btn');
+
+    hireMeBtn.addEventListener('click', function () {
+    });
+});
+document.addEventListener('DOMContentLoaded', function () {
+    const downloadCVBtn = document.querySelector('.btn2');
+
+    downloadCVBtn.addEventListener('click', function () {
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    const aboutMeHireBtn = document.querySelector('.about-content .btn');
+
+    aboutMeHireBtn.addEventListener('click', function () {
+    });
+});
